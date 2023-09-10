371fe5920b7faf872799a7ba373c303f5e5b20da
Arvind Pal <arvindpal@Arvinds-MacBook-Air.local>
2023-09-10 22:55:59 +0530

    text summary added and preview with lower case button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="/">
         {props.title}
       </a>
@@ -39,7 +39,7 @@ export default function Navbar(props) {
             placeholder="Search"
           />
           <button
-            className="btn btn-outline-success my-2 my-sm-0"
+            className="btn btn-outline- my-2 my-sm-0"
             type="submit"
           >
             Search
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,6 +6,11 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleDnClick = ()=>{
+       // console.log("Upper case was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const handleOnChange = (event)=>{
        // console.log("On change");
         setText(event.target.value)
@@ -13,12 +18,22 @@ export default function TextForm(props) {
 
     const [text, setText] = useState("Enter text here");
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-  <button className="btn btn-primary" onClick={handleUpClick} style={{marginTop:"1.5rem"}}>Convert to Uppercase</button>
+  </div>
+  <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
+  <button className="btn btn-primary mx-3" onClick={handleDnClick}>Convert to Lowercase</button>
 </div>
+<div className="container my-3">
+<h1>Your text summary</h1>
+<p>{text.split(" ").length} words and {text.length} characters</p>
+<p>{0.08 * text.split(" ").length} Minutes read</p>
+<h2>Preview</h2>
+<p>{text}</p>
     </div>
+    </>
   )
 }
